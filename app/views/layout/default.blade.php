a43357c892489953497ced96be605db53649b636
acidburn974 <djstone974@hotmail.com>
2014-07-19 21:22:33 +0400

    Suppression du lien vers le profil utilisateur

--- app/views/layout/default.blade.php
+++ app/views/layout/default.blade.php
@@ -22,7 +22,7 @@
             <div class="l-header-logo col-md-6"><h1><a href="{{ route('home') }}">{{ Config::get('other.title') }}</a></h1></div>
             <div class="l-header-user-data col-md-6">
                 @if(Auth::check())
-                    <a href="{{ route('profil', array('username' => Auth::user()->username, 'id' => Auth::user()->id)) }}" class="l-header-user-data-link">{{ Auth::user()->username }}</a>
+                    {{-- <a href="{{ route('profil', array('username' => Auth::user()->username, 'id' => Auth::user()->id)) }}" class="l-header-user-data-link">{{ Auth::user()->username }}</a> --}}
                     <span class="l-header-user-data-field">{{ Auth::user()->getUploaded() }} <i class="fa fa-long-arrow-up"></i></span>
                     <span class="l-header-user-data-field">{{ Auth::user()->getDownloaded() }} <i class="fa fa-long-arrow-down"></i></span>
                     <span class="l-header-user-data-field">{{ Auth::user()->getRatio() }} <i class="fa fa-arrows-v"></i></span>
