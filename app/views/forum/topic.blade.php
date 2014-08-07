73ead3220e91fb72223efd1ae1e79b2e0f81785d
Mister White <misterwhite974@gmail.com>
2014-08-07 19:04:09 +0400

    Ajout de la fermeture des topics

--- app/views/forum/topic.blade.php
+++ app/views/forum/topic.blade.php
@@ -44,7 +44,15 @@
         </div>
         <div class="topic-posts">
             <div class="topic-posts-head">
-                <p>{{ $topic->num_post - 1 }} replies to this topic</p>
+                <p>{{ $topic->num_post - 1 }} replies to this topic
+                    @if(Auth::check() && (Auth::user()->group->is_modo || $p->user_id == Auth::user()->id))
+                        @if($topic->state == "close")
+                            <a href="{{ route('forum_open', ['slug' => $topic->slug, 'id' => $topic->id, ])}}" class="btn btn-default btn-xs"> &#128077; Open the topic</a>
+                         @else
+                        <a href="{{ route('forum_close', ['slug' => $topic->slug, 'id' => $topic->id, ])}}" class="btn btn-default btn-xs"> &#128077; Mark it as resolved</a>
+                    @endif
+                    @endif
+                    </p>
             </div>
             @foreach($posts as $p)
                 <div class="topic-posts-p" id="post_{{ $p->id }}">
@@ -56,7 +64,10 @@
                     </article>
                     @if(Auth::check() && (Auth::user()->group->is_modo || $p->user_id == Auth::user()->id))
                         <div class="topic-moderation">
-                            <a href="{{ route('forum_post_edit', ['slug' => $topic->slug, 'id' => $topic->id, 'postId' => $p->id]) }}">Edit this post</a>
+                             @if($topic->state == "close")
+                                @else                     
+                                 <a href="{{ route('forum_post_edit', ['slug' => $topic->slug, 'id' => $topic->id, 'postId' => $p->id]) }}" class="btn btn-default btn-xs" >Edit this post</a>
+                                @endif
                         </div>
                     @endif
                 </div>
@@ -66,8 +77,11 @@
 
         <div class="topic-new-post">
             <div class="topic-new-post">
+            @if($topic->state == "close")
                 {{ Form::open(array('route' => array('forum_reply', 'slug' => $topic->slug, 'id' => $topic->id))) }}
                     <div class="from-group">
+                    <div class="col-md-12 alert alert-danger">This topic is now closed</div>
+                    @else
                         <textarea name="content" id="topic-response" cols="30" rows="10"></textarea>
                     </div>
                     @if(Auth::check())
@@ -76,6 +90,7 @@
                         <button type="submit" class="btn btn-default disabled">You must be connected</button>
                     @endif
                 {{ Form::close() }}
+                    @endif
             </div>
         </div>
     </div>
