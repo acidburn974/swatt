618881e7e8070c2c679d3c23691de917e01f45fd
Kevin Bataille <djstone974@hotmail.com>
2014-09-14 16:15:27 +0400

    4 space tab

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -1,54 +1,51 @@
 // npm install grunt grunt-contrib-less grunt-contrib-watch grunt-contrib-coffee
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    less: {
-      development: {
-        options: {
-          compress: true,
-          yuicompress: true,
-          optimization: 2
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    // target.css file: source.less file
+                    "css/main.css": "less/main.less"
+                }
+            }
         },
-        files: {
-          // target.css file: source.less file
-          "css/main.css": "less/main.less"
-        }
-      }
-    },
-    coffee: {
-        compile: {
-            options: {
-              bare: true
+        coffee: {
+            compile: {
+                options: {
+                  bare: true
+                },
+                files: {
+                  "js/forum.js": "coffee/forum.coffee",
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                nospawn: true
+                }
             },
-            files: {
-              //"js/torrents.js": "coffee/torrents.coffee",
-              //"js/login.js": "coffee/login.coffee",
-              "js/forum.js": "coffee/forum.coffee",
+            scripts: {
+                files: ['coffee/**/*.coffee'], // which files to watch
+                tasks: ['coffee'],
+                options: {
+                    nospawn: true
+                }
             }
         }
-    },
+    });
 
-    watch: {
-      styles: {
-        files: ['less/**/*.less'], // which files to watch
-        tasks: ['less'],
-        options: {
-          nospawn: true
-        }
-      },
-      scripts: {
-        files: ['coffee/**/*.coffee'], // which files to watch
-        tasks: ['coffee'],
-        options: {
-          nospawn: true
-        }
-      }
-    }
-  });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-
-  grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['watch']);
 };
